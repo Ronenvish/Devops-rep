448b70630032efc047791bea60c3be073b2a4482
Ronen Vish <ronenvish@gmail.com>
2022-11-18 10:57:26 +0200

    first commit

---
+++ Ronen.java
@@ -0,0 +1,10 @@
+public class Ronen{
+    public static void main(String[] args){
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Ronen!!!");
+            System.out.println("SAASFASGAS!@#!@#!@");
+            
+        }
+    }
+}
